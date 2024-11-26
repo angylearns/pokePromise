f5c8eeecebcc559ae40be1d8749ef2ef8e2a566c
aams91 <99359053+angylearns@users.noreply.github.com>
2024-11-26 22:38:40 +0100

    Added Abilities to list and created Cleab button function

--- src/script.js
+++ src/script.js
@@ -15,15 +15,19 @@ searchButton.addEventListener('click', function() {
         <ul>
             <li>Height: ${pokemon.height}dm</li>
             <li>Weight: ${pokemon.weight}hg</li>
-            <li>Type/s: ${pokemon.types.map(typeInfo => typeInfo.type.name)}</li>
-            <li>Move/s: ${pokemon.moves.map(moveInfo => moveInfo.move.name)}</li>
-
+            <li>Type/s: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</li>
+            <li>Abilities: ${pokemon.abilities.map(abilitiesInfo => abilitiesInfo.ability.name).join(', ')}</li>
+            <li>Move/s: ${pokemon.moves.map(moveInfo => moveInfo.move.name).join(', ')}</li>
         </ul>
         `
     })
     .catch(error => {
         console.error("Nope");
     }); 
+})
+
+cleanButton.addEventListener('click', function() {
+    mainSection.innerHTML = '';
 })
 
 const getPokemon = (id) => { 
